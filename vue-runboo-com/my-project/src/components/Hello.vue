dd69b34fc7715703216d0a47ebdc947c892637e1
zhengrun <zhengrun@zsyjr.com>
2019-02-01 16:43:26 +0800

    vue hello world example

---
+++ vue-runboo-com/my-project/src/components/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Hello World! Vue.js'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
